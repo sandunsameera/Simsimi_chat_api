990772a7eaf70354e0aa99b42ad029b3e08f9f37
sandunsameera <sandunsameera25@gmail.com>
2019-04-04 22:44:39 +0530

    Initial commit

---
+++ app/src/main/java/com/lolin/deemon_face/simsimi/MainActivity.java
@@ -0,0 +1,15 @@
+package com.lolin.deemon_face.simsimi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate (savedInstanceState);
+        setContentView (R.layout.activity_main);
+
+
+    }
+}
