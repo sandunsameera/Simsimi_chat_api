cb8fee9fa45b9a023f20d8027c9b58895e4f448c
sandunsameera <sandunsameera25@gmail.com>
2019-04-05 00:10:10 +0530

    package Helper

---
+++ app/src/main/java/com/lolin/deemon_face/simsimi/model/ChatModel.java
@@ -0,0 +1,36 @@
+package com.lolin.deemon_face.simsimi.model;
+
+public class ChatModel {
+
+    public String message;
+    public boolean isSend;
+
+    public ChatModel(String message,boolean isSend){
+        this.message = message;
+        this.isSend = isSend;
+
+    }
+
+    public ChatModel(){
+
+    }
+
+    public String getMessage(){
+        return message;
+    }
+
+    public void setMessage(String message){
+        this.message = message;
+    }
+
+    public boolean isSend(){
+        return isSend;
+
+    }
+
+    public void setSend(boolean isSend){
+
+        this.isSend = isSend;
+
+    }
+}
